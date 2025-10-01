49b0a957a21eb7aeca0f1f17e01a0b70cdcdd237
rafaellrossi <91340426+rafaellrossi@users.noreply.github.com>
2025-09-30 22:34:26 -0300

    Add files via upload

---
+++ deploy.js
@@ -0,0 +1,44 @@
+const deploy = async () => {
+  try {
+    const githubToken = process.env.TOKEN;
+    const githubRepository = process.env.GITHUB_REPOSITORY;
+
+    if (!githubToken || !githubRepository) {
+      throw new Error("Token do GitHub ou repositório não estão definidos.");
+    }
+
+    const response = await fetch(
+      `https://api.github.com/repos/${githubRepository}/dispatches`,
+      {
+        method: "POST",
+        headers: {
+          Accept: "application/vnd.github+json",
+          Authorization: `Bearer ${githubToken}`, // Token de autenticação
+          "X-GitHub-Api-Version": "2022-11-28", // Versão da API
+        },
+        body: JSON.stringify({
+          event_type: "Deploy", // Tipo de evento
+          client_payload: {
+            unit: false,
+            integration: true,
+          },
+        }),
+      }
+    );
+
+    if (!response.ok) {
+      throw new Error(
+        `Erro ao acionar o Deploy: ${response.status} ${response.statusText}`
+      );
+    }
+
+    console.log("Deploy acionado com sucesso!");
+  } catch (error) {
+    console.error(
+      "Erro ao acionar o deploy:",
+      error.response ? error.response.data : error.message
+    );
+  }
+};
+
+module.exports = deploy;
---
+++ nano.js
@@ -0,0 +1,58 @@
+const { connect } = require("puppeteer-real-browser");
+const fs = require("fs");
+require("dotenv").config({ quiet: true });
+
+const url = process.env.URL;
+const proxy = process.env.PROXY || false;
+
+const nano = async () => {
+  try {
+    const { page, browser } = await connect({
+      args: ["--start-maximized"],
+      turnstile: true,
+      headless: false,
+      proxy: proxy,
+      disableXvfb: true,
+      customConfig: {},
+      connectOption: {
+        defaultViewport: null,
+      },
+      plugins: [],
+    });
+
+    const arq = fs.readFileSync("address.txt", "utf-8").split("\n");
+    const nanoAddress = arq[Math.floor(Math.random() * arq.length)];
+
+    await page.goto(`${url}/?r=29554221350`, { waitUntil: "networkidle2" });
+    await new Promise((r) => setTimeout(r, 5000));
+    await page.goto(`${url}/nano-faucet`, { waitUntil: "networkidle2" });
+
+    await new Promise((r) => setTimeout(r, 10000));
+    await page.waitForSelector("#address");
+    await page.type("#address", nanoAddress, { delay: 20 });
+    await new Promise((r) => setTimeout(r, 5000));
+
+    const sucesso = await page.evaluate(() => {
+      const ver = document.body.innerText.includes("Success !");
+      if (ver) {
+        return document.querySelector(".ant-descriptions-item-content")
+          .innerText;
+      }
+      return null;
+    });
+
+    await page.screenshot({ path: "screen.png" });
+
+    const res = sucesso ? `Saque solicitado: ${sucesso}` : "Falha no saque";
+    console.log(res);
+    
+  } catch (error) {
+    console.error(`Erro interno do servidor: ${error.message}`);
+  } finally {
+    await browser.close();
+    await new Promise((r) => setTimeout(r, 5000));
+    await redeploy();
+  }
+};
+
+nano();
