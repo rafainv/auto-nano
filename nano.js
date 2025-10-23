bebd1de2cfbd9f63fb628289c81207538e51f1fd
rafaellrossi <91340426+rafaellrossi@users.noreply.github.com>
2025-10-22 23:42:42 -0300

    Refactor nano function to include proxy configuration
    Updated the nano function to use proxy settings from environment variables and modified the navigation flow.

--- nano.js
+++ nano.js
@@ -3,7 +3,8 @@ const fs = require("fs");
 require("dotenv").config({ quiet: true });
 
 const url = process.env.URL;
-const webProxy = process.env.WEB_PROXY;
+
+console.log(process.env.HOST_PROXY);
 
 const nano = async () => {
   const { page, browser } = await connect({
@@ -11,7 +12,12 @@ const nano = async () => {
     headless: false,
     turnstile: true,
     // disableXvfb: true,
-    // proxy: proxy
+    proxy: {
+      host: process.env.HOST_PROXY,
+      port: process.env.PORT_PROXY,
+      username: process.env.USER_PROXY,
+      password: process.env.SENHA_PROXY,
+    },
     customConfig: {},
     connectOption: {
       defaultViewport: null,
@@ -23,39 +29,23 @@ const nano = async () => {
     const arq = fs.readFileSync("address.txt", "utf-8").split("\n");
     const nanoAddress = arq[Math.floor(Math.random() * arq.length)];
 
-    await page.goto(webProxy, { waitUntil: "networkidle2" });
-    await page.waitForSelector("#url");
-    await page.type("#url", `${url}/?r=29554221350`, {
-      delay: 20,
-    });
-    await page.waitForSelector("#requestSubmit");
-    await page.click("#requestSubmit");
-    await page.waitForNavigation({ waitUntil: "networkidle2" });
-    await new Promise((r) => setTimeout(r, 10000));
-
-    const urlFaucet = page.url().replace("/?", "/nano-faucet?");
-    await page.goto(urlFaucet, { waitUntil: "networkidle2" });
-    await new Promise((r) => setTimeout(r, 10000));
-    await page.waitForSelector("#address");
-    await page.type("#address", nanoAddress, { delay: 20 });
-    // await new Promise((r) => setTimeout(r, 5000));
-    await page.waitForSelector('button[type="submit"]');
-    await page.click('button[type="submit"]');
+    await page.goto(url, { waitUntil: "networkidle2" });
+    await new Promise((r) => setTimeout(r, 5000));
+    await page.waitForSelector("#textinput_nano");
+    await page.type("#textinput_nano", nanoAddress, { delay: 20 });
+    await new Promise((r) => setTimeout(r, 5000));
+    await page.waitForSelector("#button_send");
+    await page.click("#button_send");
     await new Promise((r) => setTimeout(r, 5000));
 
     const sucesso = await page.evaluate(() => {
-      const ver = document.body.innerText.includes("Success !");
-      if (ver) {
-        return document.querySelector(".ant-descriptions-item-content")
-          .innerText;
-      }
-      return null;
+      return document.querySelector("#span_text").textContent;
     });
 
-    await page.screenshot({ path: "screen.png" });
-    const res = sucesso ? `Saque solicitado: ${sucesso}` : "Falha no saque";
-    console.log(res);
+    console.log(sucesso);
 
+    await page.screenshot({ path: "screen.png" });
+    
   } catch (error) {
     console.error(`Erro interno do servidor: ${error.message}`);
   } finally {
