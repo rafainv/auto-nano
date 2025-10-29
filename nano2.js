d83ba4cfd234c11adfeb84ba66efbe6be79da7e9
rafaellrossi <91340426+rafaellrossi@users.noreply.github.com>
2025-10-28 21:31:09 -0300

    Add nano function to interact with nanswap faucet

---
+++ nano2.js
@@ -0,0 +1,64 @@
+const { connect } = require("puppeteer-real-browser");
+const fs = require("fs");
+require("dotenv").config({ quiet: true });
+
+const url = process.env.URL_2;
+
+const nano = async () => {
+  const { page, browser } = await connect({
+    args: ["--start-maximized"],
+    headless: false,
+    turnstile: true,
+    disableXvfb: true,
+    proxy: {
+      host: process.env.HOST_PROXY,
+      port: process.env.PORT_PROXY,
+      username: process.env.USER_PROXY,
+      password: process.env.SENHA_PROXY,
+    },
+    customConfig: {},
+    connectOption: {
+      defaultViewport: null,
+    },
+    plugins: [],
+  });
+
+  try {
+    const arquivos = ["address.txt", "address.txt", "address.txt"];
+    const add = arquivos[Math.floor(Math.random() * arquivos.length)];
+    const arq = fs.readFileSync(add, "utf-8").split("\n");
+    const nanoAddress = arq[Math.floor(Math.random() * arq.length)];
+
+    await page.goto("https://nanswap.com/?r=29554221350", {
+      waitUntil: "networkidle2",
+    });
+    await new Promise((r) => setTimeout(r, 5000));
+    await page.goto("https://nanswap.com/nano-faucet", {
+      waitUntil: "networkidle2",
+    });
+    await page.waitForSelector("#address");
+    await page.type("#address", nanoAddress, { delay: 20 });
+    // await new Promise((r) => setTimeout(r, 2000));
+    try {
+      await page.waitForSelector('button[type="submit"]');
+      await page.click('button[type="submit"]');
+    } catch (error) {}
+
+    await new Promise((r) => setTimeout(r, 2000));
+
+    const sucesso = await page.evaluate(() => {
+      const suc = document.body.innerText;
+      return suc.includes("Success !");
+    });
+
+    console.log(sucesso);
+
+    await page.screenshot({ path: "screen.png" });
+  } catch (error) {
+    console.error(`Erro interno do servidor: ${error.message}`);
+  } finally {
+    await browser.close();
+  }
+};
+
+nano();
