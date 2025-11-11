e7d6068ae7107d489bdacf9b3a907b2a68a3b49d
rafaellrossi <91340426+rafaellrossi@users.noreply.github.com>
2025-11-11 19:23:54 -0300

    Update solve.py

--- solve.py
+++ solve.py
@@ -1,27 +1,9 @@
-import asyncio
-import time
-from playwright.async_api import async_playwright
-from playwright_captcha import CaptchaType, ClickSolver, FrameworkType
+from seleniumbase import sb_cdp
 
-async def solve_captcha():
-    async with async_playwright() as playwright:
-        browser = await playwright.chromium.launch(headless=True)
-        page = await browser.new_page()
-        
-        framework = FrameworkType.PLAYWRIGHT
-        
-        # Create solver before navigating to the page
-        async with ClickSolver(framework=framework, page=page) as solver:
-            # Navigate to your target page
-            await page.goto('https://clifford.io/demo/cloudflare-turnstile')
-            
-            # Solve the captcha
-            await solver.solve_captcha(
-                captcha_container=page,
-                captcha_type=CaptchaType.CLOUDFLARE_TURNSTILE
-            )
-        
-        await asyncio.sleep(10)
-        await page.screenshot(path='screen.png')
-
-asyncio.run(solve_captcha())
+url = "https://seleniumbase.io/apps/turnstile"
+sb = sb_cdp.Chrome(url)
+sb.solve_captcha()
+sb.assert_element("img#captcha-success")
+sb.set_messenger_theme(location="top_left")
+sb.post_message("SeleniumBase wasn't detected", duration=3)
+sb.driver.stop()
