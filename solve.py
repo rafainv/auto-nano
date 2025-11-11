0069885b8955a1f4b3a56389e900cefce7ef7691
rafaellrossi <91340426+rafaellrossi@users.noreply.github.com>
2025-11-11 19:42:01 -0300

    Change captcha demo URL and comment out assertions
    Updated the URL for the captcha demo and commented out some assertions and messages.

--- solve.py
+++ solve.py
@@ -1,10 +1,11 @@
 from seleniumbase import sb_cdp
 
-url = "https://seleniumbase.io/apps/turnstile"
+url = "https://clifford.io/demo/cloudflare-turnstile"
 sb = sb_cdp.Chrome(url)
 sb.solve_captcha()
-sb.assert_element("img#captcha-success")
-sb.set_messenger_theme(location="top_left")
-sb.post_message("SeleniumBase wasn't detected", duration=3)
+# sb.assert_element("img#captcha-success")
+# sb.set_messenger_theme(location="top_left")
+# sb.post_message("SeleniumBase wasn't detected", duration=3)
+sb.sleep(5)
 sb.save_screenshot("screen.png")
 sb.driver.stop()
