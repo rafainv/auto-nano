bd5f47b49f2f8300d166c9f9eb48fe9dc4efbcff
rafaellrossi <91340426+rafaellrossi@users.noreply.github.com>
2025-11-18 19:33:38 -0300

    Add Puppeteer script for automated web interaction
    This script connects to a Puppeteer browser instance, navigates to a specified URL, and interacts with a web page to submit a random address from a file, capturing the result and taking a screenshot.

---
+++ nano3.js
@@ -0,0 +1,55 @@
+const { connect } = require("puppeteer-real-browser");
+const fs = require("fs");
+require("dotenv").config({ quiet: true });
+
+const url = process.env.URL_3;
+
+const nano = async () => {
+  const { page, browser } = await connect({
+    args: ["--start-maximized"],
+    headless: false,
+    turnstile: true,
+    // disableXvfb: true,
+    proxy: {
+      host: process.env.HOST_PROXY,
+      port: process.env.PORT_PROXY,
+      username: process.env.USER_PROXY,
+      password: process.env.SENHA_PROXY,
+    },
+    customConfig: {},
+    connectOption: {
+      defaultViewport: null,
+    },
+    plugins: [],
+  });
+
+  try {
+    const arquivos = ["address.txt", "address.txt", "address.txt"];
+    const add = arquivos[Math.floor(Math.random() * arquivos.length)];
+    const arq = fs.readFileSync(add, "utf-8").split("\n");
+    const nanoAddress = arq[Math.floor(Math.random() * arq.length)];
+
+    await page.goto(url, {waitUntil: "networkidle2",});
+    await new Promise((r) => setTimeout(r, 5000));
+    await page.waitForSelector("#nanoAddr");
+    await page.type("#nanoAddr", nanoAddress, { delay: 20 });
+    await new Promise((r) => setTimeout(r, 5000));
+    await page.waitForSelector("#getNano");
+    await page.click("#getNano");
+    await new Promise((r) => setTimeout(r, 10000));
+
+    const sucesso = await page.evaluate(() => {
+      return document.querySelector(".faucetText").innerText;
+    });
+
+    console.log(sucesso);
+
+    await page.screenshot({ path: "screen.png" });
+  } catch (error) {
+    console.error(`Erro interno do servidor: ${error.message}`);
+  } finally {
+    await browser.close();
+  }
+};
+
+nano();
