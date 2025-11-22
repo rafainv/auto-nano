e9c5e81627d3947c3d561f2d7275c2da1e3d6f3d
rafaellrossi <91340426+rafaellrossi@users.noreply.github.com>
2025-11-21 23:30:13 -0300

    Add address_4.txt to arquivos array

--- nano2.js
+++ nano2.js
@@ -24,7 +24,7 @@ const nano = async () => {
   });
 
   try {
-    const arquivos = ["address.txt", "address_2.txt", "address_3.txt"];
+    const arquivos = ["address.txt", "address_2.txt", "address_3.txt", "address_4.txt"];
     const add = arquivos[Math.floor(Math.random() * arquivos.length)];
     const arq = fs.readFileSync(add, "utf-8").split("\n");
     const nanoAddress = arq[Math.floor(Math.random() * arq.length)];
